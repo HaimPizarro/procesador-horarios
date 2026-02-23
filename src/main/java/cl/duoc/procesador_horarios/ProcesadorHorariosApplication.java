db79c731389a9ee0e0f3a2fea38a7669931fc2ad
HaimPizarro <haim.pizarro@gmail.com>
2026-02-23 00:39:30 -0300

    segundo commit

---
+++ src/main/java/cl/duoc/procesador_horarios/ProcesadorHorariosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.procesador_horarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesadorHorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesadorHorariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/procesador_horarios/ProcesadorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.procesador_horarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesadorHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
